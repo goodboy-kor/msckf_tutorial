f293a68a66462bc04d8ee37be62eb6c90b687e46
goodboy-kor <llaayy.kr@gmail.com>
2022-04-13 12:33:31 +0900

    temp

--- testByWE/test.py
+++ testByWE/test.py
@@ -1,5 +1,6 @@
-import click
+import click # cli(Command Line Interface) 도구를 만들게 해주는 라이브러리
 
+# @는 데코레이터, 예약어를 만들어서 나중에 실행 할 수 있게 함
 @click.command()
 @click.option('--count', default=1, help='Number of greetings.')
 @click.option('--name', prompt='Your name',
