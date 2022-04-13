782efbe77754f3cf8004c4caac8ea2bc10b97d35
goodboy-kor <llaayy.kr@gmail.com>
2022-04-13 12:49:21 +0900

    clicklib.py add

--- testByWE/clicklib.py
+++ testByWE/clicklib.py
@@ -1,4 +1,5 @@
-import click
+# click.py로 파일명을 설정하면 라이브러리 이름과 같아서 attruibute error가 생김
+import click #cli(command line interface)도구를 만들 수 있게 도와주는 라이브러리
 
 @click.command()
 @click.option('--count', default=1, help='Number of greetings.')
